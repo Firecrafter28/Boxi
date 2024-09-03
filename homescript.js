5ed0a93548e64e3b22180f85a28f98c337ed700c
Firecrafter28 <74384569+Firecrafter28@users.noreply.github.com>
2024-09-03 00:46:52 +0000

    Update

--- homescript.js
+++ homescript.js
@@ -1,6 +1,5 @@
 // Sounds
 const ButtonPress = new Audio("Sounds/ButtonPress.wav");    // Button Press Sound
-const Glitch      = new Audio("Sounds/GlitchSound.mp3"); // Glitch Sound
 
 
 // Play Button
@@ -15,7 +14,7 @@ function Play() {
 function Help() {
     ButtonPress.play(); // Play Button Press Sound
     ButtonPress.onended = function() { // Run When Sound Ends
-        window.location.href = 'help.html'; // Go To The Help Menu
+        window.location.href = 'Help/help.html'; // Go To The Help Menu
     }
 }
 
@@ -24,13 +23,5 @@ function Changelog() {
     ButtonPress.play(); // Play Button Press Sound
     ButtonPress.onended = function() { // Run When Sound Ends
         window.location.href = 'Changelog/changelog.html'; // Go To Changelog
-    }
-}
-
-// Developer Mode Button
-function DevMode() {
-    Glitch.play(); // Play The Glitch Sound
-    Glitch.onended = function() { // Run When Sound Ends
-        window.location.href = 'DevMode/devmode.html'; // Go To Dev Mode
     }
 }
